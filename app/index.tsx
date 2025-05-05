95f36191673861b2f70a069b67924fdcff24a127
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-05-06 00:54:54 +0700

    style: :art: rnr config

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { View } from 'react-native'
+import { Button } from '~/components/ui/button'
+import { Text } from '~/components/ui/text'
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center'
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button>
+        <Text>MamaFit</Text>
+      </Button>
     </View>
-  );
+  )
 }
