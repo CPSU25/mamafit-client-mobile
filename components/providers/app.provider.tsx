387711813e7dc2b1b060d3cc34e58ff24614d25c
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-05-07 18:48:24 +0700

    feat: :sparkles: redux toolkit config

--- components/providers/app.provider.tsx
+++ components/providers/app.provider.tsx
@@ -2,6 +2,8 @@ import QueryProvider from './query.provider'
 import { useEffect } from 'react'
 import { AppState, AppStateStatus, Platform } from 'react-native'
 import { focusManager } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { store } from '~/lib/redux-toolkit/store'
 
 const onAppStateChange = (status: AppStateStatus) => {
   if (Platform.OS !== 'web') {
@@ -16,5 +18,9 @@ export default function AppProvider({ children }: { children: React.ReactNode })
     return () => subscription.remove()
   }, [])
 
-  return <QueryProvider>{children}</QueryProvider>
+  return (
+    <Provider store={store}>
+      <QueryProvider>{children}</QueryProvider>
+    </Provider>
+  )
 }
