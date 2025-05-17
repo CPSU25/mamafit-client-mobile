91ba99c9e4ba17d5242b4d76219180146a32ffda
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-05-17 14:12:41 +0700

    style: :art: sign-in + sign-up page setup

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from '~/components/ui/text'
+
+export default function SignInScreen() {
+  return (
+    <SafeAreaView>
+      <Text>SignIn</Text>
+    </SafeAreaView>
+  )
+}
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from '~/components/ui/text'
+
+export default function SignUpScreen() {
+  return (
+    <SafeAreaView>
+      <Text>SignUp</Text>
+    </SafeAreaView>
+  )
+}
