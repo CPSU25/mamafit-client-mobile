a6f5cc0cba3698ee6d68eee1a423d7c9059102d8
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-05-26 22:25:16 +0700

    style: :art: chat UI

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,10 +1,55 @@
+import * as React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import { FlatList, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
+import { Separator } from '~/components/ui/separator'
 import { Text } from '~/components/ui/text'
+import { PRIMARY_COLOR } from '~/lib/constants'
 
 export default function ChatScreen() {
+  const router = useRouter()
+
+  const handleGoBack = () => {
+    router.back()
+  }
+
+  const chats = [1, 2, 3, 4, 5]
+
   return (
-    <SafeAreaView>
-      <Text>ChatScreen</Text>
+    <SafeAreaView className='flex-1'>
+      <View className='flex flex-row items-center gap-4 p-4'>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Feather name='arrow-left' size={24} color={PRIMARY_COLOR.LIGHT} />
+        </TouchableOpacity>
+        <Text className='font-inter-medium text-xl'>Chats</Text>
+      </View>
+      <Separator />
+      <FlatList
+        data={chats}
+        renderItem={({ item, index }) => (
+          <>
+            <TouchableOpacity className='flex flex-row gap-4 items-center flex-1'>
+              <Avatar alt="Zach Nugent's Avatar" className='size-14'>
+                <AvatarImage source={{ uri: 'https://github.com/shadcn.png' }} />
+                <AvatarFallback>
+                  <Text>ZN</Text>
+                </AvatarFallback>
+              </Avatar>
+              <View className='flex flex-col flex-1'>
+                <Text className='font-inter-medium'>John Doe</Text>
+                <Text className='text-sm text-muted-foreground' numberOfLines={1}>
+                  Hello, how are you?
+                </Text>
+              </View>
+              <Text className='self-start text-xs text-muted-foreground'>12:07</Text>
+            </TouchableOpacity>
+            {index !== chats.length - 1 && <Separator className='mt-4' />}
+          </>
+        )}
+        contentContainerClassName='gap-4 p-4'
+      />
     </SafeAreaView>
   )
 }
