d7de8a34a6e6af6ce37524d69dcc4c3da7c0b080
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-07-09 13:19:47 +0700

    refactor: :art: clean code

--- app/(tabs)/canvas/create.tsx
+++ app/(tabs)/canvas/create.tsx
@@ -1,20 +1,27 @@
 import { FontAwesome } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import { FormProvider, SubmitHandler } from 'react-hook-form'
-import { TouchableOpacity, View } from 'react-native'
+import { Dimensions, TouchableOpacity, View } from 'react-native'
 import { Drawer } from 'react-native-drawer-layout'
+import AutoHeightImage from '~/components/auto-height-image'
 import SafeView from '~/components/safe-view'
 import { Button } from '~/components/ui/button'
+import { Card } from '~/components/ui/card'
 import { Text } from '~/components/ui/text'
 import DressBuilderForm from '~/features/preset/components/dress-builder-form'
 import { useGetPreset } from '~/features/preset/hooks/use-get-preset'
 import { DressBuilderFormSchema, keysToExtract } from '~/features/preset/validations'
-import { PRIMARY_COLOR } from '~/lib/constants/constants'
+import { PRIMARY_COLOR, styles } from '~/lib/constants/constants'
 import { PresetWithComponentOptions } from '~/types/preset.type'
 
 export default function CreateCanvasScreen() {
+  const router = useRouter()
   const [preset, setPreset] = useState<PresetWithComponentOptions | null>(null)
+  const [isSaving, setIsSaving] = useState(false)
   const [open, setOpen] = useState(false)
+
   const { methods, getPresetMutation } = useGetPreset()
 
   const handleSubmit: SubmitHandler<DressBuilderFormSchema> = async (data) => {
@@ -24,7 +31,25 @@ export default function CreateCanvasScreen() {
     setOpen(false)
   }
 
-  console.log(preset)
+  const handleCheckOut = async () => {
+    if (!preset) return
+
+    try {
+      setIsSaving(true)
+      await AsyncStorage.setItem(
+        'order-items',
+        JSON.stringify({
+          type: 'preset',
+          items: [preset]
+        })
+      )
+      router.push('/order/review')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsSaving(false)
+    }
+  }
 
   return (
     <SafeView>
@@ -49,13 +74,27 @@ export default function CreateCanvasScreen() {
           )
         }}
       >
-        <View className='p-4'>
-          <TouchableOpacity
-            onPress={() => setOpen((prevOpen) => !prevOpen)}
-            className='w-14 h-14 rounded-full bg-primary/10 justify-center items-center'
-          >
-            <FontAwesome name='paint-brush' size={24} color={PRIMARY_COLOR.LIGHT} />
-          </TouchableOpacity>
+        <View className='flex-1'>
+          <View className='relative'>
+            <TouchableOpacity
+              onPress={() => setOpen((prevOpen) => !prevOpen)}
+              className='w-14 h-14 rounded-full bg-primary/10 justify-center items-center absolute top-4 left-4 z-50'
+            >
+              <FontAwesome name='paint-brush' size={24} color={PRIMARY_COLOR.LIGHT} />
+            </TouchableOpacity>
+            <AutoHeightImage
+              uri={preset?.images[0] || 'https://www.tiffanyrose.com/v3-img/products/ARIDMB-zoom.jpg'}
+              width={Dimensions.get('window').width}
+            />
+          </View>
+
+          <View className='p-4 gap-2 flex-1'>
+            <Card className='p-4 flex-1' style={[styles.container]}></Card>
+
+            <Button onPress={handleCheckOut} disabled={!preset || isSaving}>
+              <Text className='font-inter-medium'>{isSaving ? 'Checking Out...' : 'Check Out'}</Text>
+            </Button>
+          </View>
         </View>
       </Drawer>
     </SafeView>
