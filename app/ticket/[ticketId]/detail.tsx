a187472c3fb4c866ba92aa48656f6b18785ca47f
DanhYeuLapTrinh <danhnguyen3923@gmail.com>
2025-08-16 11:45:43 +0700

    feat: :sparkles: route setup

---
+++ app/ticket/[ticketId]/detail.tsx
@@ -0,0 +1,10 @@
+import SafeView from '~/components/safe-view'
+import { Text } from '~/components/ui/text'
+
+export default function ViewTicketDetailScreen() {
+  return (
+    <SafeView>
+      <Text>ViewTicketDetailScreen</Text>
+    </SafeView>
+  )
+}
---
+++ app/ticket/create.tsx
@@ -0,0 +1,10 @@
+import SafeView from '~/components/safe-view'
+import { Text } from '~/components/ui/text'
+
+export default function CreateTicketScreen() {
+  return (
+    <SafeView>
+      <Text>CreateTicket</Text>
+    </SafeView>
+  )
+}
